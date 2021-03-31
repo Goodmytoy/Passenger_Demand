041d431345a1cf6793596479d7a62e060eefe973
Goodmytoy <sehop1990@gmail.com>
2021-03-31 01:48:26 +0000

    data merge

--- utils/Parallelize_DataFrame.py
+++ utils/Parallelize_DataFrame.py
@@ -1,17 +1,9 @@
-#############################################################
-# 분석과제 : 
-# 설    명 : 데이터 프레임을 병렬처리하는 함수
-# RAW DATA : 
-# 분석기법 : 
-# 작성일자 : 2020.06.01
-# 수정일자 : 2020.06.01
-# 파일버전 : v0.1
-# 작 성 자 : 박세호
-############################################################
-
 from multiprocessing_on_dill import Pool, cpu_count
+from functools import partial
+from tqdm import tqdm
 import pandas as pd
 import numpy as np
+
 
 def parallelize_dataframe(df, 
                           func, 
@@ -39,10 +31,17 @@ def parallelize_dataframe(df,
     elif group_keys is not None:
         gr_df = df.groupby(group_keys)
         df_list = [group for name, group in gr_df]
-
+    
+    tqdm.pandas()
+    #func = partial(func, **params)
+    
+    def map_func(data):
+        return data.progress_apply(func, axis = 1, **params)
+        
         
     with Pool(num_cores) as p:
-#         pd_result = pd.concat(p.map(func, df_list))
-        pd_result = p.starmap(func, **params)
+
+        pd_result = pd.concat(p.map(map_func, df_list))
+#         pd_result = p.starmap(func, **params)
         
     return pd_result
